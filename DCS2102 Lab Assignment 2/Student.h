0f4d6d106b3d90249aa33583eb171d749dca91c5
Jun2405 <j23039410@student.newinti.edu.my>
2025-11-06 18:30:03 +0800

    Update Student.h

--- DCS2102 Lab Assignment 2/Student.h
+++ DCS2102 Lab Assignment 2/Student.h
@@ -1,11 +1,20 @@
 #pragma once
 #ifndef STUDENT_H
 #define STUDENT_H
-#include "header.h"
+
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
 
 const int FullEnroll=5;
 
 class Student {
 private:
+	string name, stdId, EnrollCourse[FullEnroll];
+	int CourseCount;
+
+public:
 
 };
+
