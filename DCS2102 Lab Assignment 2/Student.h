04e4d47b8559bd8e85a689715497777f190b4cc3
Jonathan <135577194+jonathantan13@users.noreply.github.com>
2025-11-06 18:33:53 +0800

    Created student register and student login methods

--- DCS2102 Lab Assignment 2/Student.h
+++ DCS2102 Lab Assignment 2/Student.h
@@ -2,8 +2,22 @@
 #include <string>
 using namespace std;
 
-const int FullEnroll=5;
-
 class Student {
+private:
+	string password;
+	string username;
+public:
+	Student() { // Default constructor for dynamic arrays (how tedious)
+		this->username = "";
+		this->password = "";
+	};
 
+	Student(string username, string password) {
+		this->username = username;
+		this->password = password;
+	}
+
+	string getUsername() {
+		return this->username;
+	}
 };
